b37cd3552e9549fc8fc49b6102965104756ce11f
Ruchi Badkur <94377483+RuchiBadkur@users.noreply.github.com>
2024-03-15 12:57:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,20 +8,21 @@ const addItems = () => {
 	console.log(input.value);
 // First validate the item if it is not an empty string before adding it as a list item.
 // Do not allow empty submission of todo items.
-	(input.value === null || input.value === "" ||  input.value === " "  ) 
-		? alert("Please enter the task!")
-		: (
-			// After typing some text in the input and clicking Add todo button 
-			// that item should be inserted into the ordered list as a list item (li tag);
-			const li = document.createElement("li"),
-			const text = document.createTextNode(input.value),
-			li.appendChild(text),
-			list.appendChild(li),
-			
-			
-			// After clicking todo the input should be replaced with empty string.
-			input.value = "",
-		);
+	if (input.value === null || input.value === "" ||  input.value === " "  ) {
+		alert("Please enter the task!") ;
+	}else{
+
+		// After typing some text in the input and clicking Add todo button 
+		// that item should be inserted into the ordered list as a list item (li tag);
+		const li = document.createElement("li");
+		const text = document.createTextNode(input.value);
+		li.appendChild(text);
+		list.appendChild(li);
+		
+		
+		// After clicking todo the input should be replaced with empty string.
+		input.value = "";
+	}
 }
 
 addBtn.addEventListener('click', addItems);
